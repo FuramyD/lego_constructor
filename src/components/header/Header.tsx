3727ee7ff1ee3dde29a30b9cfbcf46f285cfc61f
FuramyD <ale5339587312@gmail.com>
2021-09-30 23:53:15 +0300

    made a grid for app

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.scss"
+
+export function Header() {
+    return (
+        <div className="header">
+            <div className="header__title">
+                Lego Vectary 3D
+            </div>
+            <div className="header__close-icon">
+                &times;
+            </div>
+        </div>
+    )
+}
