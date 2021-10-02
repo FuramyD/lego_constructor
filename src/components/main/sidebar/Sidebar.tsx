7dac4f99e18580cb9e10cfedfa4511e0e5aed1c2
alka0721 <aleksandr.kazantcev@netcracker.com>
2021-10-02 19:05:15 +0300

    [Sidebar]

--- src/components/main/sidebar/Sidebar.tsx
+++ src/components/main/sidebar/Sidebar.tsx
@@ -1,15 +1,34 @@
 import './Sidebar.scss';
 import '../../../index.scss';
+import {SidebarHeaderItem} from "./sidebar-header/sidebar-header-item/sidebar-header-item";
+import {TabModel} from "../../../models/ui.model";
+import {MouseEventHandler} from "react";
 
 export function Sidebar() {
+    let tabs = [
+        {id: 1, title: 'Details', isActive: true},
+        {id: 2, title: 'Import', isActive: false},
+    ];
+
     return (
         <div className="sidebar">
-            <div className="sidebar__tabs"></div>
+            <div className="sidebar__tabs">
+                {tabs.map(tab => {
+                   return <SidebarHeaderItem tab={tab} activateTab={activateTab}/>
+                })}
+
+            </div>
             <div className="sidebar__content"></div>
             <div className="sidebar__controls">
                 <button className="btn btn-default">Use selected frame</button>
                 <button className="btn btn-primary">Save view as image</button>
             </div>
         </div>
-    )
+    );
+
+    function activateTab(id: number): MouseEventHandler<HTMLDivElement> | undefined {
+        tabs = tabs.map((tab) => {
+            return tab.id === id ? {...tab, isActive: true} : {...tab, isActive: false};
+        })
+    }
 }
